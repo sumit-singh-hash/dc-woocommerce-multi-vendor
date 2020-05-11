514b4fcca2f08e534f8b7f813504898f6bdc955a
sumit-singh-hash <sumit@dualcube.com>
2020-05-11 11:44:54 +0530

    Fixed issue #294

--- templates/emails/plain/product-rejected.php
+++ templates/emails/plain/product-rejected.php
@@ -22,7 +22,7 @@ echo sprintf(  __( "Submitted by: %s",  'dc-woocommerce-multi-vendor' ), $vendor
 echo '\n';
 echo sprintf(  __( "Rejection Note: %s",  'dc-woocommerce-multi-vendor' ), $reason ); 
 echo '\n'; 
-$product_link = apply_filters( 'wcmp_email_vendor_rejected_product_link', esc_url( get_edit_post_link( $post_id )));
+$product_link = apply_filters( 'wcmp_email_vendor_rejected_product_link', esc_url(wcmp_get_vendor_dashboard_endpoint_url(get_wcmp_vendor_settings('wcmp_edit_product_endpoint', 'vendor', 'general', 'edit-product'), $post_id)));
 echo sprintf(  __( "Edit product: %s",  'dc-woocommerce-multi-vendor' ), $product_link ); 
 echo "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
 echo apply_filters( 'wcmp_email_footer_text', get_option( 'wcmp_email_footer_text' ) );
